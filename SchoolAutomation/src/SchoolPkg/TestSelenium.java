2a6fe9783d0e3d21f29e9df84a5853df8a096b88
ranjitrajgit <ranjitraj04@yahoo.com>
2018-05-13 13:22:12 +0530

    Added Login Script

--- SchoolAutomation/src/SchoolPkg/TestSelenium.java
+++ SchoolAutomation/src/SchoolPkg/TestSelenium.java
@@ -1,16 +1,18 @@
 package SchoolPkg;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.WebElement;
+import java.util.List;
 
 public class TestSelenium {
 	
 	WebDriver driver;
 	
-	
-	public void invokebrowser(){
+		public void invokebrowser(){
 		
 		try {
 			System.setProperty("webdriver.gecko.driver", "D:\\Softwares\\JAVA+Selenium\\Selenium\\geckodriver-v0.20.0-win64\\geckodriver.exe");
@@ -18,7 +20,16 @@ public class TestSelenium {
 			driver.manage().deleteAllCookies();
 			
 			driver.get("https://www.google.co.in");
-			driver.findElement(By.id("lst-ib")).sendKeys("hello world");
+			//driver.findElement(By.id("lst-ib")).sendKeys("Ranjit Kumar");
+			//driver.findElement(By.id("lst-ib")).sendKeys(Keys.ENTER);
+			//driver.findElement(By.xpath("//a[@href='https://en.wikipedia.org/wiki/Ranjit_Kumar']")).click();
+			//driver.findElement(By.linkText("Ranjit_Kumar")).click();
+			//driver.findElement(By.partialLinkText("https://en.wikipedia.org/wiki/Ranjit_Kumar")).click();
+			List<WebElement> links = driver.findElements(By.tagName("a"));
+			
+			for(WebElement ele: links)
+				System.out.println(ele.getAttribute("href"));
+			
 		} catch (Exception e) {
 			
 			e.printStackTrace();
