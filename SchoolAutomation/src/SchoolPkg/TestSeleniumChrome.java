4cec3c031fef90fde864bcf1285c75f70f301581
ranjitrajgit <ranjitraj04@yahoo.com>
2018-05-13 13:23:41 +0530

    added login script

---
+++ SchoolAutomation/src/SchoolPkg/TestSeleniumChrome.java
@@ -0,0 +1,40 @@
+package SchoolPkg;
+
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.Keys;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.*;
+	import org.openqa.selenium.remote.RemoteWebDriver;
+
+	public class TestSeleniumChrome {
+		
+		WebDriver driver;
+		
+			public void invokebrowser(){
+			
+			try {
+				System.setProperty("webdriver.gecko.driver", "D:\\Softwares\\JAVA+Selenium\\Selenium\\chromedriver_win32\\chromedriver.exe");
+				RemoteWebDriver driver = new ChromeDriver();
+				
+				driver.manage().deleteAllCookies();
+				
+				driver.get("https://www.google.co.in");
+				driver.findElement(By.id("lst-ib")).sendKeys("hello world");
+				driver.findElement(By.id("lst-ib")).sendKeys(Keys.ENTER);
+				
+			
+				
+			} catch (Exception e) {
+				
+				e.printStackTrace();
+			}
+		}
+
+		public static void main(String[] args) {
+			
+			TestSelenium myobj = new TestSelenium();
+			myobj.invokebrowser();
+			
+		}
+
+	}
